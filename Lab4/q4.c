4506e061f00ec24f036a99b71c7ac409e031b0d5
sitakanta-dev <nayaksitakanta2329@gmail.com>
2025-01-08 04:36:51 +0530

    Update q4.c

--- Lab4/q4.c
+++ Lab4/q4.c
@@ -1,58 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
-
-#define MAX_SIZE 100
-
-int is_prime(int num) {
-    if (num <= 1) return 0;
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) return 0;
-    }
-    return 1;
-}
-
-void generate_fibonacci(int *fib_array, int length) {
-    if (length >= 1) fib_array[0] = 0;
-    if (length >= 2) fib_array[1] = 1;
-
-    for (int i = 2; i < length; i++) {
-        fib_array[i] = fib_array[i - 1] + fib_array[i - 2];
-    }
-}
-
+#include <stdlib.h>
 int main() {
-    int n;
-    printf("Enter the length of the Fibonacci series: ");
-    scanf("%d", &n);
-
-    int fib_array[n]; 
-    pid_t pid = vfork();
-
-    if (pid == 0) {
-        printf("Child process with pid:%d and ppid:%d: Generating Fibonacci series...\n",getpid(),getppid());
-        generate_fibonacci(fib_array, n);
-        printf("Child process: Fibonacci series generated.\n");
-        _exit(0);
-    } else {
-        wait(NULL);
-
-        printf("Parent process: Fibonacci series:\n");
-        for (int i = 0; i < n; i++) {
-            printf("%d ", fib_array[i]);
-        }
-        printf("\n");
-
-        printf("Parent process: Prime Fibonacci numbers with positions:\n");
-        for (int i = 0; i < n; i++) {
-            if (is_prime(fib_array[i])) {
-                printf("Fibonacci[%d] = %d (Prime)\n", i, fib_array[i]);
-            }
-        }
-
-        printf("Parent process with pid:%d and ppid:%d: Task completed.\n",getpid(),getppid());
+    int length;
+    printf("Enter the length: ");
+    scanf("%d", &length);
+    int fibArray[length];
+    if (vfork() == 0) {
+        fibArray[0] = 0;
+        fibArray[1] = 1;
+        for (int i = 2; i < length; i++)
+            fibArray[i] = fibArray[i-1] + fibArray[i-2];
+        exit(0);
     }
-
+    wait(NULL);
+    printf("Fibonacci series: ");
+    for (int i = 0; i < length; i++)
+        printf("%d ", fibArray[i]);
+    printf("\nPrime Fibonacci numbers:\n");
+    for (int i = 0; i < length; i++) {
+        int isPrime = 1;
+        if (fibArray[i] <= 1)
+            isPrime = 0;
+        for (int j = 2; j * j <= fibArray[i]; j++)
+            if (fibArray[i] % j == 0) {
+                isPrime = 0;
+                break;
+            }
+        if (isPrime)
+            printf("%d at index %d\n", fibArray[i], i);
+    }
     return 0;
 }
